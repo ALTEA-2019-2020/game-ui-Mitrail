fe0528095a3720a2bc1ac8ce0297af31cd294d52
edouard guegain <edouardguegain@gmail.com>
2020-02-10 10:54:44 +0100

    WIP : localization

--- src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -3,9 +3,16 @@ package com.miage.altea.game_ui.pokemonTypes.service;
 import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,8 +24,11 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
     String pokemonServiceUrl;
 
     public List<PokemonType> listPokemonsTypes() {
-        PokemonType[] types = restTemplate.getForObject(pokemonServiceUrl + "/pokemon-types/", PokemonType[].class);
-        return types != null ? Arrays.asList(types) : new ArrayList<>();
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setAcceptLanguageAsLocales(Arrays.asList(LocaleContextHolder.getLocale()));
+        HttpEntity requestEntity = new HttpEntity<>(requestHeaders);
+        ResponseEntity<PokemonType[]> types = restTemplate.exchange(pokemonServiceUrl + "/pokemon-types/", HttpMethod.GET, requestEntity, PokemonType[].class);
+        return types.getBody() != null ? Arrays.asList(types.getBody()) : new ArrayList<>();
     }
 
     @Autowired
